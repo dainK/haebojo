6d6c26432550450c2e497152025eaef823c7d176
da <dain940209@gmail.com>
2023-10-24 18:51:43 +0900

    improt

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvkQMypGDR1HHLoU_c4g-JEqIfb0NMw60",
+    authDomain: "sparta-eight.firebaseapp.com",
+    projectId: "sparta-eight",
+    storageBucket: "sparta-eight.appspot.com",
+    messagingSenderId: "1083363522950",
+    appId: "1:1083363522950:web:707cdbd5905fa43d839407",
+    measurementId: "G-STP1V2J94X"
+  };
--- info.js
+++ info.js
@@ -1,11 +1,4 @@
-
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OWFhNTcxMjUwMjAwZGFkZmQyNGIzYmQyNGMzNzU1ZiIsInN1YiI6IjY1MmY0NDk0ZWE4NGM3MDBhZWY0YWQxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z0256uYeUXzk9kL0zAHc4r9nsqgTAcZa3Kw5tXUX7Ek'
-    }
-};
+import {options} from "./tmdbOption.js" ;
 
 const currentUrl = window.location.href;
 const urlParts = currentUrl.split('?');
