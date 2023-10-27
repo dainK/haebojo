14031952b0ed609a2ea6d237990bab91a4e8b03b
da <dain940209@gmail.com>
2023-10-27 13:34:48 +0900

    코드 수정

--- script/movie.js
+++ script/movie.js
@@ -22,7 +22,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   user.signOutButton.addEventListener("click", createSignOutModal);
-
+  user.signupButton.addEventListener("click", createSignModalElement);
+  user.pwChangeButton.addEventListener("click", pwChange);
   // 로그인 버튼 클릭 시 로그인 모달 열기
   user.loginButton.addEventListener("click", () => {
     if (!!sessionStorage.getItem("user")) {
@@ -250,11 +251,10 @@ function createSignModalElement() {
   });
 
   sign_cancel.addEventListener("click", () => {
-    sign_modal.style.display = "none";
+    // sign_modal.style.display = "none";
+    sign_modal.remove();
   });
 }
-
-createSignModalElement();
 
 // 회원탈퇴
 async function createSignOutModal() {
@@ -375,13 +375,14 @@ const pwChange = () => {
         alert("아이디 또는 비밀번호가 틀렸습니다.");
       }
     });
+
     cancel_btn.addEventListener("click", () => {
-      changePwModal.style.display = "none";
+      // changePwModal.style.display = "none";
+      changePwModal.remove();
     });
   });
 };
 
-pwChange();
 
 // 페이지에 영화 목록 생성 함수
 function createPage(pageData) {
@@ -464,6 +465,7 @@ function createMovieCard(data) {
   overviewEl.addEventListener("mouseenter", () => {
     overviewEl.innerText = data.overview;
   });
+  
   overviewEl.addEventListener("mouseleave", () => {
     overviewEl.innerText = truncatedOverview;
   });
